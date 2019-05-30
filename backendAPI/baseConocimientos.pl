e3f1b0a53ba9bfe36ae70ec3fb994e4776099ee9
mabo <mabo.daniel@gmail.com>
2019-05-29 23:18:10 -0600

    Agregada una receta de prueba con los endpoints

--- backendAPI/baseConocimientos.pl
+++ backendAPI/baseConocimientos.pl
@@ -7,3 +7,11 @@ receta("Huevo Cocido", "Tentempié").
 ingredienteReceta("Huevo", "Huevo Frito").
 ingredienteReceta("Huevo", "Huevo Cocido").
 listaPasos(["Abrir el huevo", "Colocar Huevo en Sartén"], "Huevo Frito").
+receta("Blin","food").
+ingrediente("Chicken Produce").
+ingredienteReceta("Chicken Produce","Blin").
+listaPasos(["Open chicken","Crare blin","Eat"],"Blin").
+ingrediente("FLOUR").
+ingredienteReceta("FLOUR","Blin").
+ingrediente("milk").
+ingredienteReceta("milk","Blin").
